fb2e8b0bc79ee5e2a2b97f6c9003b74bf2c2dd30
jsneooms <65919245+jsneooms@users.noreply.github.com>
2020-10-13 20:40:53 -0300

    commit

--- t8.py
+++ t8.py
@@ -7,11 +7,24 @@ app = Flask(__name__)
 @app.route('/')
 def nao_entre_em_panico():
 
-
-    primos = "Tudo vai dar certo, caros alunos!"
-
-
-    return primos
+ limite = 1000
+ numero = 2
+ c = 1
+ p = 0
+ print "Primos: ",
+ while numero < 1000:
+     i = numero -1
+     while i > 1:
+         if numero % i == 0: break
+           i -= 1
+           c += 1
+     else:
+        print numero,
+         p += 1
+     numero += 1
+ 
+print "\n\nForam encontrados %d números primos." %p
+print "Foram necessárias %d comparações." %c
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
