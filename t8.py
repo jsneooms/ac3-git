60532e805533d8645eea2b7b277f09c572a706b6
jsneooms <65919245+jsneooms@users.noreply.github.com>
2020-10-13 21:23:56 -0300

    Finalizando assim

--- t8.py
+++ t8.py
@@ -1,42 +1,17 @@
-import os, sys
+import os
 from flask import Flask, jsonify, request
 from math import sqrt
 
 app = Flask(__name__)
 
 @app.route('/')
+def nao_entre_em_panico():
 
-def is_prime(number):
-    divisions = 0
-    response = False
-    number = int(number)
-    if number:
-        for i in range(1, number + 1):
-            if number % i == 0:
-                divisions = divisions + 1
-            if divisions == 2:
-                response = True
-    if divisions > 2:
-        response = not response
-    return response
- 
-if __name__ == '__main__':
-    while(True):
-        os.system('clear')
-        print 'PROGRAMA QUE VERIFICA E MOSTRA QUAIS OS NÚMEROS PRIMOS DE UM INTERVALO NUMÉRICO DE 1 ATÉ 100.'
-        quantidade = int( raw_input('QUANTIDADE DE NÚMEROS A SEREM VERIFICADOS: ') )
- 
-        if quantidade == 0:
-            break
- 
-        for numero in range(1, quantidade + 1):
-            resposta = is_prime(numero)
-            if resposta:
-                print '%d => É INTEIRO' % numero
-            else:
-                print numero
-    sys.stdin.read(1)
-    os.system('clear')
+
+    primos = "Tudo vai dar certo, caros alunos!"
+
+
+    return primos
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
