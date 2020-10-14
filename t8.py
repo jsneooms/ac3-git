267e8eea39e52a22f66155f8096a8d2330da0fd3
jsneooms <65919245+jsneooms@users.noreply.github.com>
2020-10-13 21:06:48 -0300

    commit again

--- t8.py
+++ t8.py
@@ -5,13 +5,26 @@ from math import sqrt
 app = Flask(__name__)
 
 @app.route('/')
-def nao_entre_em_panico():
 
+num = 100
+def primo(num):
+   if num != 1:
+         t = True
+         for i in range(2,num):
+             if num % i == 0:
+                 t = False
+                 break
+         return t
+ 
+for num in range(2, lim + 1):
+   test = primo(num)
+   if (test):
+       e.append(num)
+   else:
+       not_p.append(num)
 
-    primos = "Olá, sou o Reinaldo"
-
-
-    return primos
+print "Números primos: ", e
+print "Não são primos: ", not_p
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
