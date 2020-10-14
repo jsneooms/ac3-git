becf910b0b936320ac057747ff4b7cba1e472fde
jsneooms <65919245+jsneooms@users.noreply.github.com>
2020-10-13 21:10:46 -0300

    por favor vai

--- t8.py
+++ t8.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from flask import Flask, jsonify, request
 from math import sqrt
 
@@ -6,25 +6,37 @@ app = Flask(__name__)
 
 @app.route('/')
 
-num = 100
-def primo(num):
-   if num != 1:
-         t = True
-         for i in range(2,num):
-             if num % i == 0:
-                 t = False
-                 break
-         return t
+def is_prime(number):
+    divisions = 0
+    response = False
+    number = int(number)
+    if number:
+        for i in range(1, number + 1):
+            if number % i == 0:
+                divisions = divisions + 1
+            if divisions == 2:
+                response = True
+    if divisions > 2:
+        response = not response
+    return response
  
-for num in range(2, lim + 1):
-   test = primo(num)
-   if (test):
-       e.append(num)
-   else:
-       not_p.append(num)
-
-print "Números primos: ", e
-print "Não são primos: ", not_p
+if __name__ == '__main__':
+    while(True):
+        os.system('clear')
+        print 'PROGRAMA QUE VERIFICA E MOSTRA QUAIS OS NÚMEROS PRIMOS DE UM INTERVALO NUMÉRICO DE 1 ATÉ 100.'
+        quantidade = int( raw_input('QUANTIDADE DE NÚMEROS A SEREM VERIFICADOS: ') )
+ 
+        if quantidade == 0:
+            break
+ 
+        for numero in range(1, quantidade + 1):
+            resposta = is_prime(numero)
+            if resposta:
+                print '%d => É INTEIRO' % numero
+            else:
+                print numero
+    sys.stdin.read(1)
+    os.system('clear')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
